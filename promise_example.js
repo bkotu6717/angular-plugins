5221ec8a70f36d7af449317bfa8d664e02ebfa34
Bhaskar <bkotu6717@gmail.com>
2016-10-29 09:35:56 +0530

    add more info by comments text

--- promise_example.js
+++ promise_example.js
@@ -8,14 +8,23 @@ function getData($timeout, $q) {
 
      	// if the rounded number is not 0 we resolve promise, if 0 we reject promise
       if(Math.round(Math.random())) {
-        defer.resolve('promise resolved!!')
+      	// the parameter of the resolve/reject will be sent as promise result
+      	// the call back thereafter identifies to execute success/error callbacks
+        
+      	// executes success call back of the caller
+        defer.resolve('promise resolved!!');
       } else {
-        defer.reject('promise rejected!!')
+        
+      	// executes error call back of the caller
+        defer.reject('promise rejected!!');
+
       }
     }, 2000)
 
-    // return the promise to the caller before execution completion of current method
-    return defer.promise
+    // return the promise to the caller before execution completion of called method
+    // so that caller will continue without waiting for the response of the called function
+    // when the caller recieves a promise response, it will be executing it's respective call back function(success/error)
+    return defer.promise;
   }
 }
 
@@ -23,8 +32,10 @@ app.factory('getData', getData)
 .run(function(getData) {
   var promise = getData()
 	.then(function(string) { // promise resolved callback
-	  console.log(string);
+	  console.log("executed success callback as the promise gave resolved response:: ", string);
 	}, function(string) { // promise rejected callback
-		console.log(string);
-	});
+		console.log("executed error callback as the promise gave rejected response:: ",string);
+	}).finally(function() { // Finally block will be executed regardless of promise resolved or rejected
+      console.log('Finished at:', new Date())
+    });
 })
